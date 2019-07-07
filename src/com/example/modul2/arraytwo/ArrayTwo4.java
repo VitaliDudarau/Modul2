82687443008b81e58362a88defebe7b2cdd19092
VitaliDudarau <dudarev.vitalii@gmail.com>
2019-07-07 11:00:00 +0300

    Change 7 on 6,  ArrayTwo4 Modul2

--- src/com/example/modul2/arraytwo/ArrayTwo4.java
+++ src/com/example/modul2/arraytwo/ArrayTwo4.java
@@ -14,7 +14,7 @@ public class ArrayTwo4 {
 
     public static void main(String[]args) {
 
-        int[][] arr = new int[7][7];
+        int[][] arr = new int[6][6];
 
         for (int i = 0; i < arr.length; i += 2) {
             for (int j = 0; j < arr[i].length; j++) {
