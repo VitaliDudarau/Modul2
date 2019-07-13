bb4949d1426654ae0145c1c2c31c8bfaa741ca0e
VitaliDudarau <dudarev.vitalii@gmail.com>
2019-07-13 11:03:29 +0300

    Add ArrayTwo15 Modul2

--- src/com/example/modul2/arraytwo/ArrayTwo15.java
+++ src/com/example/modul2/arraytwo/ArrayTwo15.java
@@ -9,16 +9,49 @@ public class ArrayTwo15 {
     @SuppressWarnings("resources")
     public static void main(String[]args) {
 
-        int m, n;
-        int[][] arr = new int[7][8];
+        int m = 21, n = -19, max = n;
+        int[][] arr = new int[3][5];
 
-        for (int j = arr[0].length - 1; j >= 0; j--) {
-            for (int i  = 0; i <= j; i++) {
-                arr[i][j] = 1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (int) (Math.random()*(m - n + 1)) + n;
             }
         }
 
         System.out.println("arr[][] = ");
+        System.out.println("---------------------------------------" +
+                "-----------------------------------------" +
+                "-----------------------------------------");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j  = 0; j < arr[i].length; j++) {
+                System.out.print("[" + i + "][" + j + "] = " + arr[i][j] + " | ");
+            }
+            System.out.println("\n---------------------------------------" +
+                    "-----------------------------------------" +
+                    "-----------------------------------------");
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] % 2 != 0) {
+                    arr[i][j] = max;
+                }
+            }
+        }
+
+        System.out.println("maximum = " + max);
+        System.out.println("---------------------------------------" +
+                "-----------------------------------------" +
+                "-----------------------------------------");
+        System.out.println("changed arr[][] = ");
         System.out.println("---------------------------------------" +
                 "-----------------------------------------" +
                 "-----------------------------------------");
